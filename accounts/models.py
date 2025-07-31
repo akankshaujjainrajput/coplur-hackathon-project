d477bf670d76ae8b1073e7c2a1fa548f46bbcfe4
Akanksha Ujjain <akankshaujjainrajput2308@gmail.com>
2025-07-31 21:00:42 +0530

    feat: Create data migration to seed initial admin user

---
+++ accounts/models.py
@@ -0,0 +1,10 @@
+# In accounts/models.py
+from django.db import models
+from django.contrib.auth.models import User
+
+class Profile(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    role = models.CharField(max_length=10, choices=[('student', 'Student'), ('admin', 'Admin')], default='student')
+
+    def __str__(self):
+        return f'{self.user.username} - {self.role}'
